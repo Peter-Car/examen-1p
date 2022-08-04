5d6bde07ef6150784cb289b6e652cc3ec282ae4e
Peter-Car <petercarpioa@gmail.com>
2022-08-04 16:09:29 -0500

    se suve el proyecto

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>verificar si el numero es perfecto</title>
+    </head>
+    
+    <body>
+       <style>
+            
+            header{
+                
+               background:#2ECC71;
+               
+   
+            }
+            
+            
+            #tabla{
+               
+                margin-left: 60px;
+                
+   
+            }
+            
+            .form-group{
+              
+                
+                width: 800px;
+                margin-right: 40px;
+                
+            }
+            
+            .container{
+                text-align: center;
+                margin-top: 30px;
+                margin-left: 40px;
+                display: flex;
+                font-size: 16px;
+                background: #2ECC71;
+            }
+            
+        </style>
+        <div class="container">
+            
+            <form>
+                
+                <div class="form-group">
+                     <p>
+                        <label>numero1:</label> <br>
+                        <input type="float" id="numero1"  placeholder="Ingrese el numero 1"> <br>
+                    </p>
+                    
+                    
+                    <p>
+                        
+                        <button onclick="alert('su numero no es perfecto')" > verificar si es perfecto</button>
+                        
+                        
+                    </p>
+                    
+                    <input type="edidtext" id="enviar"  placeholder=""++numero1> <br>
+                    
+    </body>
+</html>
